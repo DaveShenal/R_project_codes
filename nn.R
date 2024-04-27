c23ce7adff86d1260dbc9289af4da2de42660752
Dave Shenal <dave.20221546@iit.ac.lk>
2024-04-27 23:53:10 +0530

    fixed the  MAPE calculation error

--- nn.R
+++ nn.R
@@ -1,4 +1,5 @@
 # Load libraries
+library(readxl)
 library(neuralnet)
 library(forecast)
 
@@ -63,9 +64,8 @@ lag_options <- c(1, 2, 3, 4)
 io_matrices <- lapply(lag_options, function(lag) create_io_matrices(normalized_training_data, lag))
 io_matrices_test <- lapply(lag_options, function(lag) create_io_matrices(normalized_testing_data, lag))
 
-str(io_matrices_test[[2]]$output)
-
 # 3 Building and Evaluating MLP Models
+
 
 # Define function to train and evaluate an MLP model
 train_and_evaluate_model <- function(input_matrix, output_matrix, hidden_layers, nodes_per_layer, activation_function, testing_data) {
@@ -77,22 +77,18 @@ train_and_evaluate_model <- function(input_matrix, output_matrix, hidden_layers,
                      hidden = c(rep(nodes_per_layer, hidden_layers)),
                      linear.output = TRUE, 
                      act.fct = activation_function)
-  print("model trained")
   
   # Make predictions on testing data
   predictions <- predict(model, newdata = testing_data$input)
-  print("got predictions")
   
-  # Calculate evaluation metrics
+
   # Calculate evaluation metrics
   rmse <- sqrt(mean((predictions - testing_data$output)^2))
-  print("rmse calculated")
   mae <- mean(abs(predictions - testing_data$output))
-  print("mae calculated")
-  mape <- mean(abs(predictions - testing_data$output) / testing_data$output) * 100
-  print("mape calculated")
+  # Calculate MAPE (Mean Absolute Percentage Error)
+  mape <- mean(ifelse(testing_data$output != 0, 
+                      abs(predictions - testing_data$output) / testing_data$output * 100, 0))
   smape <- mean(abs(predictions - testing_data$output) / (abs(predictions) + abs(testing_data$output)) * 200)
-  print("smape calculated")
   
   
   return(list(model = model, rmse = rmse, mae = mae, mape = mape, smape = smape))
@@ -104,9 +100,9 @@ for (lag in lag_options) {
   input_matrix <- io_matrices[[lag]]$input  # Access input matrix for current lag
   output_matrix <- io_matrices[[lag]]$output  # Access output matrix for current lag
   testing_data <- io_matrices_test[[lag]] # Access output matrix
-  for (hidden_layers in c(1, 2)) {
-    for (nodes in c(5, 10)) {
-      for (activation in c("logistic", "tanh")) {
+  for (hidden_layers in c(2)){
+    for (nodes in c(8)){
+      for (activation in c("tanh", "logistic")) {
         # Call the function to train and evaluate the model
         model_result <- train_and_evaluate_model(input_matrix, output_matrix, hidden_layers, nodes, activation, testing_data)
         # Append the model results to a list for each configuration
@@ -130,6 +126,4 @@ for (model_name in names(models)) {
   }
   cat("SMAPE:", models[[model_name]]$smape, "\n\n")
 }
-
-print(length(models))
 
